cd33fe101f3a49a38325e80a405544f19a5f3526
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-08-10 00:54:53 -0400

    Added the CalculatorButton_Click shared event handler

--- COMP123-S2017-Lesson12B/CalculatorForm.cs
+++ COMP123-S2017-Lesson12B/CalculatorForm.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Windows.Forms;
 /*
  * Name: Bal Krishna Dhakal
  * StudentID: 300916314
  * Date: August 3, 2017
  * Desciption: Calculator Demo Project
- * Version: 0.2- Added the form closing event handler
- */
+ * Version: 0.4-Created a shared event handler for the operator Buttons
+ */ 
 namespace COMP123_S2017_Lesson12B
 {
     public partial class CalculatorForm : Form
@@ -30,6 +32,28 @@ namespace COMP123_S2017_Lesson12B
         private void CalculatorForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+        /// <summary>
+        /// This is the shared event handler for the calculator Buttons
+        /// Not including the operator Buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            Button calculatorButton = sender as Button; // downcasting
+            ResultTextBox.Text += calculatorButton.Text;
+
+           // Debug.WriteLine("A Calculator Button was clicked");
+        }
+        /// <summary>
+        /// This is a shared event handler for the FormClosing event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OperatorButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
